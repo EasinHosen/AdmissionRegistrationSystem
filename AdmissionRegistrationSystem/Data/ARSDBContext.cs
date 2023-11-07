0457047766df3a439b9434accf30f78eadfd607b
Easin Md. Hosen <easinhosen5@gmail.com>
2023-11-07 10:21:45 +0600

    feat: home logo added title changed

---
+++ AdmissionRegistrationSystem/Data/ARSDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AdmissionRegistrationSystem.Models;
+
+namespace AdmissionRegistrationSystem.Data
+{
+    public class ARSDBContext : DbContext
+    {
+
+    }
+}
