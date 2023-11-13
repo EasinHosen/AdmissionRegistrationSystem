df7d5c078264230783ae96cfbe3254c7cf1a7927
Easin Md. Hosen <easinhosen5@gmail.com>
2023-11-13 10:43:21 +0600

    feat: admin login implemented

---
+++ AdmissionRegistrationSystem/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using AdmissionRegistrationSystem.Data;
+using AdmissionRegistrationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace AdmissionRegistrationSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ARSDBContext _context;
+
+        public LoginController(ARSDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(LoginModel login) { 
+            var authenticate = await _context.Logins.FirstOrDefaultAsync(
+                m => m.UserName == login.UserName && m.Password == login.Password);
+            if (authenticate == null)
+            {
+                return View();
+            }
+            //Debug.Print(authenticate.ToString());
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
