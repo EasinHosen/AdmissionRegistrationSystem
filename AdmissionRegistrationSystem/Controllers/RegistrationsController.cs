f1174c71239e97075af6298a8327c7351b8e1f6b
Easin Md. Hosen <easinhosen5@gmail.com>
2023-11-23 22:13:14 +0600

    feat: validation in html, file choose

--- AdmissionRegistrationSystem/Controllers/RegistrationsController.cs
+++ AdmissionRegistrationSystem/Controllers/RegistrationsController.cs
@@ -21,21 +21,53 @@ namespace AdmissionRegistrationSystem.Controllers
             return View();
         }
 
+        /*[HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase imageFile)
+        {
+            if (imageFile != null && imageFile.ContentLength > 0)
+            {
+                // Process the uploaded image
+                var fileName = Path.GetFileName(imageFile.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
+                imageFile.SaveAs(path);
+
+                // Save the image file information to the database or perform any necessary processing
+                // ...
+
+                return RedirectToAction("Index"); // Redirect to the index action after successful upload
+            }
+            else
+            {
+                // Handle any errors or invalid file uploads
+                return View("Error"); // Redirect to an error page if no file was uploaded
+            }
+        }*/
+
+        [HttpPost]
+        public ActionResult ProcessImage()
+        {
+            try
+            {
+                IFormFile file = Request.Form.Files[0];
+
+                // Process the file as needed
+                // For example, save the file or perform other operations
+                Debug.Print("got the file!");
+                
+                return Json(new { success = true, message = "Image processed successfully", url= "https://firebasestorage.googleapis.com/v0/b/let-s-chat-16cb3.appspot.com/o/ars%2Fplaceholder.png?alt=media&token=58df47c8-2773-4f21-8f60-de7b029e1280" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"Error processing image: {ex.Message}" });
+            }
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> IndexAsync(RegistrationModel registrationModel) {
 
-            registrationModel.regId = Guid.NewGuid();
-            registrationModel.PresAddress = registrationModel.PermAddress;
-            registrationModel.PresAddress.AddressType = "Present";
-            registrationModel.PermAddress.AddressType = "Permanent";
-            registrationModel.SSC.ExamType = "SSC";
-            registrationModel.HSC.ExamType = "HSC";
-            registrationModel.PhotoUrl = "https://firebasestorage.googleapis.com/v0/b/let-s-chat-16cb3.appspot.com/o/ars%2Fplaceholder.png?alt=media&token=58df47c8-2773-4f21-8f60-de7b029e1280";
+            Debug.Print("start");
 
-            Debug.Print(registrationModel.ToJson());
-            /*Debug.Print("start");
-            Debug.Print(
-               registrationModel.ToJson());
             if (ModelState.IsValid)
             {
                 if (registrationModel == null)
@@ -44,10 +76,20 @@ namespace AdmissionRegistrationSystem.Controllers
                 }
                 Debug.Print("valid");
 
-                Debug.Print(registrationModel.ToJson());
-                Debug.Print(registrationModel.Name);
+               Debug.Print(registrationModel.ToJson());
 
-            }*/
+            }
+            else {
+                Debug.Print("Invalid");
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        Debug.Print($"Error: {error.ErrorMessage}");
+                    }
+                }
+
+            }
             return View();
         }
     }
