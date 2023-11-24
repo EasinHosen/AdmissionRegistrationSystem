fe3f075f194c54f692e4ceec67f6b2d7e7b73b77
Easin Md. Hosen <easinhosen5@gmail.com>
2023-11-24 21:18:02 +0600

    feat: reg list view for admin created

--- AdmissionRegistrationSystem/Controllers/AdminPanelController.cs
+++ AdmissionRegistrationSystem/Controllers/AdminPanelController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdmissionRegistrationSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdmissionRegistrationSystem.Controllers
 {
     public class AdminPanelController : Controller
     {
-        public IActionResult Index()
+        private readonly ARSDBContext _context;
+
+        public AdminPanelController(ARSDBContext context) {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.Registrations != null ? View(await _context.Registrations.ToListAsync()) : Problem("No data found!!");
+
+
         }
     }
 }
