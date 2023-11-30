c736d2d59657e4801bea5b03c6cad1dca9f3a843
Easin Md. Hosen <easinhosen5@gmail.com>
2023-11-30 08:12:22 +0600

    feat: admin view payment added

--- AdmissionRegistrationSystem/Controllers/AdminPanelController.cs
+++ AdmissionRegistrationSystem/Controllers/AdminPanelController.cs
@@ -1,6 +1,9 @@
 ﻿using AdmissionRegistrationSystem.Data;
+using AdmissionRegistrationSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+using System.Diagnostics;
 
 namespace AdmissionRegistrationSystem.Controllers
 {
@@ -18,7 +21,30 @@ namespace AdmissionRegistrationSystem.Controllers
             {
                 return Unauthorized();
             }
-            return _context.Registrations != null ? View(await _context.Registrations.ToListAsync()) : Problem("No data found!!");
+            var rList = await _context.Registrations.ToListAsync();
+
+            List<AdminViewDataModel> dataList = new List<AdminViewDataModel>();
+            for (int i = 0; i < rList.Count; i++)
+            {
+                var dataM = new AdminViewDataModel();
+                dataM.registrations = rList[i];
+
+                var hasPayment = _context.PaymentInfos.Any(p => p.RegistrationId == rList[i].Id);
+                if (hasPayment)
+                {
+                    dataM.paymentStatus = "Completed";
+                    var item = await _context.PaymentInfos.FirstOrDefaultAsync(e=>e.RegistrationId == rList[i].Id);
+                    dataM.transaactionId = item.transactionId;
+                }
+                else
+                {
+                    dataM.paymentStatus = "Pending";
+                    dataM.transaactionId = null;
+                }
+                dataList.Add(dataM);
+            }
+
+            return _context.Registrations != null ? View(dataList) : Problem("No data found!!");
 
         }
     }
---
+++ AdmissionRegistrationSystem/Models/AdminViewDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AdmissionRegistrationSystem.Models
+{
+    public class AdminViewDataModel
+    {
+        public RegistrationModel registrations { get; set; }
+        public string paymentStatus { get; set; }
+
+        public Guid? transaactionId { get; set; }
+    }
+}
